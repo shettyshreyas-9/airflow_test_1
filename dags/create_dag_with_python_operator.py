8b6e813c80d9f755fafd5aa283bc3b60bb69ffe9
unknown <lcladmin@PG0392KD.springernature.com>
2024-08-25 01:22:23 +0530

    Use python operator to run function and pass parameters to it

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -9,12 +9,12 @@ default_args={
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet():
-    print("Hello World")
+def greet(name,age):
+    print(f"Hello World! My name is {name} and I am {age} years old.")
 
 with DAG(
     default_args= default_args,
-    dag_id= 'our_first_dag_with_python_operator_v1',
+    dag_id= 'our_first_dag_with_python_operator_v2',
     description= 'Our first dag using python operator',
     start_date= datetime(2024,8,25, 4),
     schedule_interval='@daily'
@@ -23,7 +23,8 @@ with DAG(
     
     task_1= PythonOperator(
         task_id='greet',
-        python_callable= greet
+        python_callable= greet,
+        op_kwargs={'name':'Steven Strange', 'age':50}
     )
 
     task_1
